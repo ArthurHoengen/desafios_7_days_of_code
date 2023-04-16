c46bc13a1388561bfd5e6b080e78b678e2b37c85
ArthurHoengen <arthurhoengen@gmail.com>
2023-04-16 13:55:40 -0300

    Adicionando o desafio extra ao código

--- Desafio_2.js
+++ Desafio_2.js
@@ -3,3 +3,16 @@ const idade = prompt("Quantos anos você tem?");
 const linguagem = prompt("Qual linguagem de programação você esta estudando?");
 const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
 alert(msg);
+
+//Bônus:
+
+const opiniao = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+if(opiniao == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}
+else if(opiniao == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
+else{
+    alert("Resposta inválida");
+}
